6483b9641f950eae8083c789073ba2b15f4f0eab
Daniel Caruso II <d.m.c.ii.az@gmail.com>
2016-04-13 00:13:40 -0700

    stopsegfltingplz:(

--- my_syscall.c
+++ my_syscall.c
@@ -1,30 +1,22 @@
 #include <linux/syscalls.h>
 #include <linux/kernel.h>    /* Needed for KERN_INFO */
 #include <linux/list.h>
-#include <linux/sched.h>
-#include <linux/tty.h> /* Needed to access TTY stuff */
-#include <linux/jiffies.h> /* Needed for time */
+
+
 #include <linux/printk.h> /* Needed to print to the kernal log file */
-#include <asm/uaccess.h>
-#include <linux/slab.h>
-#include <linux/string.h>
+
+
+
 #include <linux/mm.h>
 #include <linux/mm_types.h>
 #include <linux/types.h>
-//#include <linux/shmem_fs.h>
-//#include <linux/swapops.h>
- 
- 
-struct data
-{
-   int aval;
-   long address;
 
-};
+#include <asm/pgtable.h>
+#include <linux/highmem.h>
 
-#define CHECK_BIT(var,pos) ((var) & (1<<(pos)))
 
-asmlinkage long sys_my_syscall(void* pnt, int pid, const unsigned long address)
+
+asmlinkage long sys_my_syscall( int pid, const unsigned long address)
 {
    
    struct task_struct* task;
@@ -33,9 +25,7 @@ asmlinkage long sys_my_syscall(void* pnt, int pid, const unsigned long address)
    pud_t* pud;
    pmd_t* pmd;
    pte_t* pte;
-   struct page *page;
-   printk(KERN_INFO "adfgdfrgsfd\n");
-   struct data *outputData = kmalloc(sizeof(struct data),__GFP_NOFAIL);
+   
    printk(KERN_INFO "ADDRESS: %lu\n", address);
    for_each_process(task)
    {
@@ -44,7 +34,7 @@ asmlinkage long sys_my_syscall(void* pnt, int pid, const unsigned long address)
             pr_info("IN\n");            
             printk(KERN_INFO "Task %d found\n", task->pid);
             mm = task->mm;
-            page = NULL;
+            
             pgd = pgd_offset(mm, address);
  
             printk(KERN_INFO "PGD INFO: PRESENT: %d, NONE: %d, BAD: %d\n", pgd_present(*pgd), pgd_bad(*pgd), pgd_none(*pgd));
@@ -57,47 +47,42 @@ asmlinkage long sys_my_syscall(void* pnt, int pid, const unsigned long address)
                    
                 if(!(pud_none(*pud) || pud_bad(*pud)) && pud_present(*pud))
                 {
-                   
                     pmd = pmd_offset(pud, address);
                     printk(KERN_INFO "PMD INFO: PRESENT: %d, NONE: %d, BAD: %d\n", pmd_present(*pmd), pmd_bad(*pmd), pmd_none(*pmd));
                    
                     if(!(pmd_none(*pmd) || pmd_bad(*pmd)) && pmd_present(*pmd))
                     {
-                         
-                        pte = pte_offset_kernel(pmd, address);
-                        printk(KERN_INFO "PTE INFO: PRESENT: %p\n", &pte);
- 						//#define CHECK_BIT(var,pos) ((var) & (1<<(pos)))
+                        printk(KERN_INFO " after pmd if before pte = pre offset"); 
+                        pte = pte_offset_map(pmd, address);
+                        printk(KERN_INFO "PTE INFO: PRESENT: %d\n", pte_present(*pte));
 
-                        //if(!CHECK_BIT(pte,63))
-                        if(pte)
+                        if(pte_present(*pte))
                         {
-                        	page = pte_page(*pte);
-                        	if(pte_present(*pte))
-                        	{
-                            	
-                           		outputData->address = pte_pfn(*pte);
-                            	outputData->aval = 1;
-                            	copy_to_user(pnt, outputData, sizeof(struct data));
-                            	return 0;
-                        	}
-                    
-                        	else
-                        	{
-                        		outputData->address = __pte_to_swp_entry(*pte).val;
-                           		//aval is set to swap.
-                            	outputData->aval = 2;
-                            	copy_to_user(pnt, outputData, sizeof(struct data));
-                            	return 0;
-                        	}
+                          printk(KERN_INFO " after pte if "); 
+
+                          if(!pte_none(*pte))
+                          {
+                            printk(KERN_INFO " !pte_none(*pte) \n"); 
+
+                            return pte->pte;
+                            //return 0;
+                          }
+                          else
+                          {
+                            printk(KERN_INFO " in else \n"); 
+
+                              return __pte_to_swp_entry(*pte).val;
+                            //return 0;
+                          }
+                          
                         }
                     }
                 }
             }
-            outputData->aval = 0;
+
             printk(KERN_INFO "Data not found!\n");
-            copy_to_user(pnt, outputData, sizeof(struct data));
-            return 0;
         }
     }
+    printk(KERN_INFO "Data not found!\n");
     return 0;
 }
--- mytestcall.c
+++ mytestcall.c
@@ -6,56 +6,30 @@
 #include <stdlib.h> 
 #define __NR_sys_get_addr 359
  
-struct data
-{
-    int aval;
-    unsigned long address;
-
-};
  
 int main(int argc, char* argv[])
 {
-	unsigned long vfn;
-	//unsigned long physical;
+    //unsigned long vfn;
+    long pleaseWork =0 ;
+    //unsigned long physical;
     unsigned long temp = strtoul(argv[2],NULL,16);
+    printf("argc: %d\n", argc);
     if(argc == 3)
     {
-        struct data *pointer = malloc(sizeof(struct data));
-        // argv[1] is the pid
-        // argv[2] is the virtual memory address
-        syscall(__NR_sys_get_addr, pointer, atoi(argv[1]),temp) ;
-        // Output data
-        if(pointer->aval == 1) // in memory 
-        {
-        	//(? << 12) | (0b111111111111 & address)
 
-        	vfn = (0xfffff000 & temp) >> 12;
-        	printf("%08x\n",temp);
-        	//printf("%08x\n",pointer->address + 0xC00000000);
-        	printf("%08x\n",vfn);
-        	//printf("%08x\n",0b111111111111);
+        printf("before\n");
+        pleaseWork = syscall(__NR_sys_get_addr, atoi(argv[1]),temp) ;
+        printf("after\n");
+ 
+        pleaseWork = ( 0xfffff000 & pleaseWork);
+        printf("%lu\n",pleaseWork);
 
+        printf("0x%x\n",pleaseWork);
 
-
-            printf("PFN: %lu\tVFN: %lu\tPHYSICAL: 0x%x\n", pointer->address, vfn,(pointer->address << 12) |  vfn );
-
-        }
-        else if(pointer->aval == 2) // swap 
-        {
-        	vfn = (0xfffff000 & temp) >> 12;
-        	printf("VFN: %lu\tSWAP: %lu\n", vfn, pointer->address);
-        }
-        else
-        {
-            printf("not available\n");
-           // exit(0);
-            return 0;
-        }
-       
     }
     else
     {
         printf("ERROR: Two Arguments expected! Number of Arguments:%d\n\n", (argc - 1));
     }
-    return 0;
+
 }
